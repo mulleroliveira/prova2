eb05d0d0ec11acf6e62b6c0ce17eb0e9056b0291
Aluno <you@example.com>
2019-07-09 20:26:29 -0300

    Finish

--- gestao/urls.py
+++ gestao/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('listagem/', views.users),
+    path('listagem/delete/<int:id>/', views.delete)
 ]
--- gestao/views.py
+++ gestao/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required
+def users(request):
+    users = User.objects.all()
+    return render(request, 'usuario/listagem.html', {"users":users})
+
+
+@permission_required("delete_user", login_url="/gestao/listagem/")
+@login_required
+def delete(request, id):
+    user = User.objects.all().get(pk=id).delete()
+    messages.success(request, "Operação concluida")
+    return redirect("/gestao/listagem/")
